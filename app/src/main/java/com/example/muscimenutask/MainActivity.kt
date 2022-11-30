0dc30a5d97077f490201eb4dfd65fe5e7a2eb01a
hadaw kakazai <hadsawkakazaihk@gmail.com>
2022-11-30 12:28:57 +0500

    complete

--- app/src/main/java/com/example/muscimenutask/MainActivity.kt
+++ app/src/main/java/com/example/muscimenutask/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.muscimenutask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
+    override fun onCreateOptionsMenu(
+        menu: Menu
+    ) : Boolean{
+
+        val inflator = menuInflater.inflate(R.menu.mainmenu,menu)
+        return true
+    }
+
+
+
+
 }
